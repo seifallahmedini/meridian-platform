60664568912a602c0e891ce21323014c5998bc34
seif allah medini <seifallahmedini@gmail.com>
2026-07-28 21:19:09 +0100

    MLP-18: add /shipments/new, /shipments/new/:id, /shipments/drafts routes (protected)

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,11 +1,40 @@
 import { createBrowserRouter } from 'react-router'
+import { ProtectedRoute } from '@/auth/ProtectedRoute'
 import { AppShell } from '@/layouts/AppShell'
 import { SampleWidgetsPage } from '@/pages/SampleWidgetsPage'
+import { DraftsListPage } from '@/pages/shipments/DraftsListPage'
+import { NewShipmentPage } from '@/pages/shipments/NewShipmentPage'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppShell />,
-    children: [{ index: true, element: <SampleWidgetsPage /> }],
+    children: [
+      { index: true, element: <SampleWidgetsPage /> },
+      {
+        path: 'shipments/new',
+        element: (
+          <ProtectedRoute>
+            <NewShipmentPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: 'shipments/new/:id',
+        element: (
+          <ProtectedRoute>
+            <NewShipmentPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: 'shipments/drafts',
+        element: (
+          <ProtectedRoute>
+            <DraftsListPage />
+          </ProtectedRoute>
+        ),
+      },
+    ],
   },
 ])
